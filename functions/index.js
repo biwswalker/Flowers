a573fd4a86a5f85e79e81ea15d7f99ff5f1c480d
biwswalker <jannarong.san@gmail.com>
2017-10-28 00:23:47 +0700

    Biws

--- functions/index.js
+++ functions/index.js
@@ -1,79 +1,18 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
-// const sendgrid = require('sendgrid')
-// const client = sendgrid("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")
+exports.httpMailGun = functions.https.onRequest((req, res) => {
+  let api_key = 'key-20b037c15421f85bea6d6d0a2e9315c0';
+  let domain = 'sandbox9e1a705e25bd48fc9cbad3d9d02edbab.mailgun.org';
+  let mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
-const sendgrid = require('sendgrid')(
-  process.env.SENDGRID_API_KEY || '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'
-);
+  var data = {
+    from: 'Mail Gun TutsDaddy <postmaster@sandbox9e1a705e25bd48fc9cbad3d9d02edbab.mailgun.org>',
+    to: 'jannarong.san@gmail.com',
+    subject: 'WTF',
+    text: 'Test WTF THIS Is.'
+  }
 
-function parseBody(body) {
-  var helper = sendgrid.mail;
-  var fromEmail = new helper.Email(body.from);
-  var toEmail = new helper.Email(body.to);
-  var subject = body.subject;
-  var content = new helper.Content('text/html', body.content);
-  var mail = new helper.Mail(fromEmail, subject, toEmail, content);
-  return mail.toJSON();
-}
-
-exports.httpEmail = functions.https.onRequest((req, res) => {
-  res.send("GGWP");
-  res.send(req.body);
-  // return Promise.resolve()
-  //   .then(() => {
-  //     console.log(req.method);
-  //     if (req.method !== 'POST') {
-  //       const error = new Error('Only POST requests are accepted');
-  //       error.code = 405;
-  //       throw error;
-  //     }
-
-  //     const request = client.emptyRequest({
-  //       method: 'POST',
-  //       path: '/v3/mail/send',
-  //       body: parseBody(req.body)
-  //     });
-
-  //     return client.API(request);
-  //   }).then((response) => {
-  //     if (response.body) {
-  //       res.send(response.body);
-  //     } else {
-  //       res.end();
-  //     }
-  //   }).catch((err) => {
-  //     console.error(err);
-  //     return Promise.reject(err);
-  //   })
-})
-
-
-
-
-function sendMail() {
-  const mailRequest = sg.emptyRequest({
-    method: 'POST',
-    path: '/v3/mail/send',
-    body: {
-      personalizations: [{
-        to: [{ email: 'jannarong.san@gmail.com' }],
-        subject: 'Greenwich Test'
-      }],
-      from: { email: 'noreply@flowers-flow.firebaseapp.com' },
-      content: [{
-        type: 'text/plain',
-        value: 'Hello Joe! Can you hear me! Is the line still getting through?'
-      }]
-    }
+  mailgun.messages().send(data, function (error, body) {
+    console.log(body);
   });
-
-  sg.API(mailRequest, function (error, response) {
-    if (error) {
-      console.log('Mail not sent; see error message below.');
-    } else {
-      console.log('Mail sent successfully!');
-    }
-    console.log(response);
-  });
-}
+});
--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -1,37 +1,47 @@
-import { Injectable } from '@angular/core';
-import { Http, Headers, Response, URLSearchParams } from '@angular/http';
-import 'rxjs/add/operator/toPromise';
+import { Injectable } from "@angular/core";
+import { Http, Headers, Response, URLSearchParams } from "@angular/http";
+import "rxjs/add/operator/toPromise";
 
 @Injectable()
 export class EmailService {
-
-  constructor(private http: Http) { }
+  constructor(private http: Http) {}
 
   mailUrl = "https://us-central1-flowers-flow.cloudfunctions.net/sendmail";
 
   onSubmitForm() {
-    let headers = new Headers({ 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' });
-    return this.http.post(this.mailUrl, JSON.stringify({ gg: 'biwswalker' }), { headers: headers }).subscribe();
+    let headers = new Headers({
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    });
+    return this.http
+      .post(this.mailUrl, JSON.stringify({ gg: "biwswalker" }), {
+        headers: headers
+      })
+      .subscribe();
   }
 
   sendEmail() {
     let url = "https://us-central1-flowers-flow.cloudfunctions.net/httpEmail";
+    let headers = new Headers({
+      'Content-Type': 'application/json',
+      'Access-Control-Request-Method': 'POST',
+      'Access-Control-Allow-Origin': '*'
+    });
     let params: URLSearchParams = new URLSearchParams();
-    let headers = new Headers({ 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' });
     params.set('to', 'jannarong.san@gmail.com');
     params.set('from', 'noreply@flowers-flow.firebaseapp.com');
     params.set('subject', 'test-email');
     params.set('content', 'Hello World');
 
-    return this.http.post(url, params, { headers: headers }).subscribe();
-    // return this.http.post(url, params, { headers: headers })
-    // .toPromise()
-    // .then(res => {
-    //   console.log(res)
-    // })
-    // .catch(err => {
-    //   console.log(err)
-    // })
-
+    // return this.http.post(url, params, { headers: headers }).subscribe();
+    return this.http
+      .post(url, params, { headers: headers })
+      .toPromise()
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      })
   }
 }
